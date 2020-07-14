ae6bf9cc395cb812607fc438a919c06c9f490c73
Zaid Akhter <zaidakhter1202@gmail.com>
2020-07-14 12:18:47 +0500

    📦 NEW: Initial lots of stuff

---
+++ src/components/footer.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <p>footer</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,16 +1,11 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
+import "./layout.scss"
 
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
-import "./layout.css"
+import Footer from "./footer"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -25,20 +20,10 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
+      <Header />
+      <div className="page">
+        <main className="main container">{children}</main>
+        <Footer />
       </div>
     </>
   )
