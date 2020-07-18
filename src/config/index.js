41c87458170a4b035bac1aa998a1a7bf18e29273
Zaid Akhter <zaidakhter1202@gmail.com>
2020-07-18 22:44:46 +0500

    📦 NEW: Add config

---
+++ src/config/index.js
@@ -0,0 +1,51 @@
+module.exports = {
+  siteTitle: "Zaid Akhter | WebDeveloper",
+  siteDescription:
+    "I am a Fullstack Developer from Karachi, PK who specializes in building eye-catching and blazing-fast websites using modern web technologies.",
+  siteKeywords:
+    "Zaid Akhter, Zaid, Akhter, zaidakhterr, front-end engineer, web developer, full-stack developer, javascript, react.js, node.js",
+  siteUrl: "",
+  siteLanguage: "en_US",
+  name: "Zaid Akhter",
+  location: "Karachi, PK",
+  email: "zaidakhter1202@gmail.com",
+  github: "https://github.com/zaidakhterr",
+  twitterHandle: "@zaidakhterr",
+  socialLinks: [
+    {
+      name: "GitHub",
+      url: "https://github.com/zaidakhterr",
+    },
+    {
+      name: "Linkedin",
+      url: "https://www.linkedin.com/in/zaidakhterr/",
+    },
+    {
+      name: "Twitter",
+      url: "https://twitter.com/zaidakhterr",
+    },
+    {
+      name: "Facebook",
+      url: "https://www.facebook.com/zaidakhterr",
+    },
+  ],
+
+  navLinks: [
+    {
+      name: "About",
+      url: "/#about",
+    },
+    {
+      name: "Experience",
+      url: "/#jobs",
+    },
+    {
+      name: "Work",
+      url: "/#projects",
+    },
+    {
+      name: "Contact",
+      url: "/#contact",
+    },
+  ],
+}
