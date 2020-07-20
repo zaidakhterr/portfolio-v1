8cf1cb46ee0dc43f8d7616c093c5881465ef8fb6
Zaid Akhter <zaidakhter1202@gmail.com>
2020-07-20 11:38:11 +0500

    📦 NEW: Add articles from DEV.to

--- src/components/sections/blog.js
+++ src/components/sections/blog.js
@@ -1,6 +1,20 @@
 import { graphql, useStaticQuery } from "gatsby"
 import React from "react"
 
+const Article = ({ article }) => {
+  console.log(article)
+  return (
+    <div className="article">
+      <img src={article.cover_image} alt={article.title} />
+      <h3>{article.title}</h3>
+      {article.tags.map(tag => (
+        <span className="tag">{tag}</span>
+      ))}
+      <p>{article.description}</p>
+    </div>
+  )
+}
+
 const Blog = () => {
   const data = useStaticQuery(graphql`
     {
@@ -21,6 +35,9 @@ const Blog = () => {
   return (
     <>
       <h1>Blog</h1>
+      {data.allDevArticles.nodes.map(node => (
+        <Article article={node.article} />
+      ))}
     </>
   )
 }
