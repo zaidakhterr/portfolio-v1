3caf4ad55ba85ea774e7ba6f7379befa9e48bcbf
Zaid Akhter <zaidakhter1202@gmail.com>
2020-07-20 11:39:14 +0500

    👌 IMPROVE: Add link to og article

--- src/components/sections/blog.js
+++ src/components/sections/blog.js
@@ -4,14 +4,16 @@ import React from "react"
 const Article = ({ article }) => {
   console.log(article)
   return (
-    <div className="article">
-      <img src={article.cover_image} alt={article.title} />
-      <h3>{article.title}</h3>
-      {article.tags.map(tag => (
-        <span className="tag">{tag}</span>
-      ))}
-      <p>{article.description}</p>
-    </div>
+    <a href={article.url} target="_blank" rel="nofollow noopener noreferrer">
+      <div className="article">
+        <img src={article.cover_image} alt={article.title} />
+        <h3>{article.title}</h3>
+        {article.tags.map(tag => (
+          <span className="tag">{tag}</span>
+        ))}
+        <p>{article.description}</p>
+      </div>
+    </a>
   )
 }
 
