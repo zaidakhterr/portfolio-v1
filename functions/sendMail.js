3250c119ccde595fbfae1dae61498b1ef8a74822
Zaid Akhter <zaidakhter1202@gmail.com>
2020-07-20 23:54:12 +0500

    📦 NEW: sendMail function

---
+++ functions/sendMail.js
@@ -0,0 +1,35 @@
+require("dotenv").config()
+const { MAIL_LOGIN, MAIL_PASSWORD } = process.env
+
+exports.handler = function (event, context, callback) {
+  let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: MAIL_LOGIN,
+      pass: MAIL_PASSWORD,
+    },
+  })
+
+  const { email, subject, message } = event.body
+
+  transporter.sendMail(
+    {
+      from: MAIL_LOGIN,
+      to: email,
+      subject: subject,
+      text: message,
+    },
+    function (error, info) {
+      if (error) {
+        callback(error)
+      } else {
+        callback(null, {
+          statusCode: 200,
+          body: "Ok",
+        })
+      }
+    }
+  )
+}
