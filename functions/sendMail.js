11f4e2b1c7a9b28a4db65f16b9f06600a3d8c7e7
Zaid Akhter <zaidakhter1202@gmail.com>
2020-07-21 13:41:57 +0500

    👌 IMPROVE: hahahahahah

--- functions/sendMail.js
+++ functions/sendMail.js
@@ -5,7 +5,9 @@ const { MAIL_LOGIN, MAIL_PASSWORD } = process.env
 
 exports.handler = function (event, context, callback) {
   let transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
     auth: {
       user: MAIL_LOGIN,
       pass: MAIL_PASSWORD,
