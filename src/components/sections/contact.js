9b1976c409d15ed90ce53d85cfb5658e34d510a3
Zaid Akhter <zaidakhter1202@gmail.com>
2020-07-23 06:29:01 +0500

    📦 NEW: Basic form layout and styles

--- src/components/sections/contact.js
+++ src/components/sections/contact.js
@@ -1,12 +1,61 @@
-import React from "react"
+import React, { useState } from "react"
 
 const Contact = () => {
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [subject, setSubject] = useState("")
+  const [message, setMessage] = useState("")
+
   return (
     <div>
       <div className="section-heading">
         <h2>Contact Me</h2>
         <span></span>
       </div>
+      <p className="section-description">
+        Discuss a project or just want to say hi? My inbox is open for all.
+      </p>
+      <form className="contact-form">
+        <div className="input-group">
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            value={name}
+            onChange={e => setName(e.target.value)}
+          />
+        </div>
+        <div className="input-group">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="input-group">
+          <label htmlFor="subject">Subject</label>
+          <input
+            type="text"
+            name="subject"
+            id="subject"
+            value={subject}
+            onChange={e => setSubject(e.target.value)}
+          />
+        </div>
+        <div className="input-group">
+          <label htmlFor="message">Message</label>
+          <textarea
+            name="message"
+            id="message"
+            value={message}
+            onChange={e => setMessage(e.target.value)}
+          />
+        </div>
+      </form>
     </div>
   )
 }
