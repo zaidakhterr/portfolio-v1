46db27a7afa268b47ba66668495b10f20e382897
Zaid Akhter <zaidakhter1202@gmail.com>
2020-07-28 11:31:59 +0500

    📦 NEW: 404 page styles

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,3 +1,4 @@
+import { Link } from "gatsby"
 import React from "react"
 
 import Layout from "../components/layout"
@@ -6,8 +7,35 @@ import SEO from "../components/seo"
 const NotFoundPage = () => (
   <Layout>
     <SEO />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div
+      className="container"
+      style={{
+        height: "calc(100vh - 56px - 120px)",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <h1
+        style={{
+          fontSize: "min(4rem, calc(2.5rem + 1vw))",
+          marginBottom: "1rem",
+        }}
+      >
+        <span style={{ color: "#323330" }}>404</span> NOT FOUND
+      </h1>
+      <p
+        style={{
+          marginBottom: "2rem",
+        }}
+      >
+        You just hit a route that doesn&#39;t exist... the sadness.
+      </p>
+      <Link to="/">
+        <button className="btn">Go home</button>
+      </Link>
+    </div>
   </Layout>
 )
 
